3b31feac9eafb267186c1d307fd136f12262459b
annpodsh <anyutkapodshivalova@mail.ru>
2022-04-01 11:21:26 +1000

    Change task_hw4_4.py

--- hw4/tasks/task_hw4_4.py
+++ hw4/tasks/task_hw4_4.py
@@ -32,38 +32,52 @@ Instruction:
  - Right-click on the doctest file
  - Click Run 'doctests in <filename>'
 
- >>> fizzbuzz(5)
-['1', '2', 'fizz', '4', 'buzz']
->>> fizzbuzz(20)
-['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizz buzz', '16', '17', 'fizz', '19', 'buzz']
-
 * https://en.wikipedia.org/wiki/Fizz_buzz
 ** Энциклопедия профессора Фортрана page 14, 15, "Робот Фортран, чисть картошку!"
 
 """
+import doctest
 from typing import List
 import string
 
 
-"""
-in a list with a sequence of numbers from 1 to n+1, we make changes: 
-divisible by 3, return the string 'Fizz';
-divisible by 5, return the string 'Buzz';
-divisible by both 3 and 5, return the string 'FizzBuzz';
-and if divisible by neither 3 nor 5, simply return x.
-"""
+
+#in a list with a sequence of numbers from 1 to n+1, we make changes:
+#divisible by 3, return the string 'Fizz';
+#divisible by 5, return the string 'Buzz';
+#divisible by both 3 and 5, return the string 'FizzBuzz';
+#and if divisible by neither 3 nor 5, simply return x.
+
+
+
 
 def fizzbuzz(n: int) -> List[str]:
-    list = []
-    for x in range(1, n + 1):
-        if x % 15 == 0:
-            list.append("fizz buzz")
-        elif x % 3 == 0:
-            list.append("fizz")
-        elif x % 5 == 0:
-            list.append("buzz")
-        else:
-            list.append(str(x))
-    return list
+    """
+ >>> fizzbuzz(5)
+ ['1', '2', 'fizz', '4', 'buzz']
 
-print(fizzbuzz(20))
+ >>> fizzbuzz(8)
+ ['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8']
+
+ >>> fizzbuzz(15)
+ ['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizzbuzz']
+
+ :param n: numbers 1 to n
+ :return: List[str] where each el % 3 == 0 = fizz and each el % 5 == 0 = buzz
+    """
+    list_fizzbuzz: List[str] = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            list_fizzbuzz.append("fizzbuzz")
+        elif i % 3 == 0:
+            list_fizzbuzz.append("fizz")
+        elif i % 5 == 0:
+            list_fizzbuzz.append("buzz")
+        else:
+            list_fizzbuzz.append(f"{i}")
+
+    return list_fizzbuzz
+
+
+if __name__ == '__main__':
+    doctest.testmod()
