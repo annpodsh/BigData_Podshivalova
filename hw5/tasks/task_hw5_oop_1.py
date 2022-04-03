78bb336f7a643c6fc4ed341251e9699602eff344
annpodsh <anyutkapodshivalova@mail.ru>
2022-04-03 17:21:10 +1000

    Add task_hw5_oop_1.py and task_hw5_save_original_info.py to hw5/tasks

---
+++ hw5/tasks/task_hw5_oop_1.py
@@ -0,0 +1,111 @@
+"""
+Необходимо создать 3 класса и взаимосвязь между ними (Student, Teacher,
+Homework)
+Наследование в этой задаче использовать не нужно.
+Для работы с временем использовать модуль datetime
+
+1. Homework принимает на вход 2 атрибута: текст задания и количество дней
+на это задание
+Атрибуты:
+    text - текст задания
+    deadline - хранит объект datetime.timedelta с количеством
+    дней на выполнение
+    created - c точной датой и временем создания
+Методы:
+    is_active - проверяет не истело ли время на выполнение задания,
+    возвращает boolean
+
+2. Student
+Атрибуты:
+    last_name
+    first_name
+Методы:
+    do_homework - принимает объект Homework и возвращает его же,
+    если задание уже просрочено, то печатет 'You are late' и возвращает None
+
+3. Teacher
+Атрибуты:
+     last_name
+     first_name
+Методы:
+    create_homework - текст задания и количество дней на это задание,
+    возвращает экземпляр Homework
+    Обратите внимание, что для работы этого метода не требуется сам объект.
+
+PEP8 соблюдать строго.
+Всем перечисленным выше атрибутам и методам классов сохранить названия.
+К названием остальных переменных, классов и тд. подходить ответственно -
+давать логичные подходящие имена.
+"""
+import datetime
+
+
+"""
+Homework class, accept text of the task and time deadline attributes,
+and have is_active() method, that returns boolean, if the task execution time has expired.
+"""
+
+class Homework:
+    def __init__ (self, text: str , deadline: datetime.timedelta):
+        self.text=text
+        self.deadline=deadline
+        self.created=datetime.datetime.now()
+
+    def is_active(self):
+        data=self.created+self.deadline
+        return self.created <= data
+
+
+"""
+Student class, accept last_name and first_name attributes,
+and have create_homework() method,
+that takes task text and integer number of days left for doing this homework, and returns Homework instance,
+that contains task text and timedelta instance with days left
+"""
+
+class Student:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def do_homework(self, homework: Homework):
+        if homework.is_active():
+            return homework
+        else:
+            print("You are late")
+            return None
+
+
+"""
+Teacher class, accept last_name and first_name attributes,
+and have method create_homework, that the text of the task and the number of days for this task,
+returns an instance of Homework
+"""
+
+class Teacher:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def create_homework(self, task: str, days_left: int) -> Homework:
+        return Homework(task, datetime.timedelta(days_left))
+
+
+if __name__ == '__main__':
+    teacher = Teacher('Daniil', 'Shadrin')
+    student = Student('Roman', 'Petrov')
+    teacher.last_name  # Daniil
+    student.first_name  # Petrov
+
+    expired_homework = teacher.create_homework('Learn functions', 0)
+    expired_homework.created  # Example: 2019-05-26 16:44:30.688762
+    expired_homework.deadline  # 0:00:00
+    expired_homework.text  # 'Learn functions'
+
+    # create function from method and use it
+    create_homework_too = teacher.create_homework
+    oop_homework = create_homework_too('create 2 simple classes', 5)
+    oop_homework.deadline  # 5 days, 0:00:00
+
+    student.do_homework(oop_homework)
+    student.do_homework(expired_homework)  # You are late
---
+++ hw5/tasks/task_hw5_save_original_info.py
@@ -0,0 +1,67 @@
+"""
+Написать декоратор который позволит сохранять информацию из
+исходной функции (__name__ and __doc__), а так же сохранит саму
+исходную функцию в атрибуте __original_func
+
+print_result изменять нельзя, за исключением добавления вашего
+декоратора на строку отведенную под него - замените комментарий
+
+До применения вашего декоратор будет вызываться AttributeError при custom_sum.__original_func
+Это корректное поведение
+После применения там должна быть исходная функция
+
+Ожидаемый результат:
+print(custom_sum.__doc__)  # 'This function can sum any objects which have __add___'
+print(custom_sum.__name__)  # 'custom_sum'
+print(custom_sum.__original_func)  # <function custom_sum at <some_id>>
+"""
+
+import functools
+
+"""
+the decorator_info decorator saves information from
+the original function (__name__ and __doc__), and saves the
+original function itself in the __original_func attribute
+"""
+
+def decorator_info(original_func):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            return result
+
+        wrapper.__original_func = original_func
+        wrapper.__doc__ = original_func.__doc__
+        wrapper.__name__ = original_func.__name__
+        return wrapper
+
+    return decorator
+
+
+def print_result(func):
+    @decorator_info(original_func=func)
+    def wrapper(*args, **kwargs):
+        """Function-wrapper which print result of an original function"""
+        result = func(*args, **kwargs)
+        print(result)
+        return result
+
+    return wrapper
+
+
+@print_result
+def custom_sum(*args):
+    """This function can sum any objects which have __add___"""
+    return functools.reduce(lambda x, y: x + y, args)
+
+
+if __name__ == "__main__":
+    custom_sum([1, 2, 3], [4, 5])
+    custom_sum(1, 2, 3, 4, 5, 6)
+
+    print(custom_sum.__doc__)
+    print(custom_sum.__name__)
+    without_print = custom_sum.__original_func
+
+    # the result returns without printing
+    without_print(1, 2, 3, 4)
